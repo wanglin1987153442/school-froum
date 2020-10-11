1184a83c65e0e75f377dd5ed439364aa9d008c70
Administrator <admin@example.com>
2020-10-11 22:25:29 +0800

    父子项目搭建 以及jar生成

---
+++ api/src/test/java/com/niit/songshanyuan/api/ApiApplicationTests.java
@@ -0,0 +1,14 @@
+package com.niit.songshanyuan.api;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
